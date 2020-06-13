410dd604fb8e608396f44a0e3df2978247b048cb
Lauriel Mesquita <laurielmesquita@me.com>
2020-06-12 21:43:26 -0300

    :arrow_up: Upgrading dependencies

--- src/App.js
+++ src/App.js
@@ -22,22 +22,20 @@ function App () {
     setRepositories([...repositories, response.data])
   }
 
-  async function handleRemoveRepository(id) {
+  async function handleRemoveRepository (id) {
     await api.delete(`repositories/${id}`)
 
-    setRepositories(repositories.filter(
-      repository => repository.id !== id
-    ))
+    setRepositories(repositories.filter(repository => repository.id !== id))
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
+      <ul data-testid='repository-list'>
         {repositories.map(repository => (
           <li key={repository.id}>
             {repository.title}
             <button onClick={() => handleRemoveRepository(repository.id)}>
-            Remover
+              Remover
             </button>
           </li>
         ))}
